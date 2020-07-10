0b41e5b1b26931775536db50eeee3d1ebc5cab4e
leucopterus <strelchenkoedgar@gmail.com>
2020-07-10 12:38:38 +0300

    flask-jwt, authentication and some sql examples

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'asdf-9213uf-cHJN(_#@yfhco#@*p:a='
 
+'''
 stores_list = [
     {
         'name': 'My Wonderful Store',
@@ -59,18 +64,67 @@ def items(name):
             if store['name'] == name:
                 return jsonify({'items': store['items']})
     return jsonify({'message': 'store not found'})
+'''
+# -------------------------------------------------------------------------
 
 
 api = Api(app)
+jwt = JWT(app, authenticate, identity)
+
+items_list = []
 
 
-class Student(Resource):
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help='This field cannot be left blank!'
+    )
+
+    @jwt_required()
     def get(self, name):
-        return {'student': name}
+        item = next(filter(lambda x: x['name'] == name, items_list), None)
+        return {'item': item}, 200 if item else 404
+
+    @jwt_required()
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items_list), None):
+            return {'message': f'An item with name {name} is already exists'}, 400
+        data = self.parser.parse_args()
+        item = {'name': name, 'price': data['price']}
+        items_list.append(item)
+        return item, 201
+
+    @jwt_required()
+    def delete(self, name):
+        global items_list
+        items_list = list(filter(lambda x: x.get('name') != name, items_list))
+        return {'message': 'item deleted'}
+
+    @jwt_required()
+    def put(self, name):
+        data = self.parser.parse_args()
+
+        item = next(filter(lambda x: x['name'] == name, items_list), None)
+        if item is None:
+            item = {'name': name, 'price': data.get('price')}
+            items_list.append(item)
+        else:
+            item.update(data)
+        return item
 
 
-api.add_resource(Student, '/student/<string:name>')
+class ItemList(Resource):
+    @jwt_required()
+    def get(self):
+        return {'items': items_list}
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items/')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
